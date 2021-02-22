e53e88e6f2fc24df813e670cd511c1edc00fe4dd
xnopasaranx <toe@mausbrand.de>
2021-02-22 11:23:53 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,10 @@ sys.path.insert(0, os.path.abspath('..'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'autoapi.extension'
 ]
+
+autoapi_dirs = ['../flare']
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
